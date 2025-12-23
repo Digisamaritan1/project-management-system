35a13ae6d26635860b95062ebf4c17ab7f7438ec
Parth Detroja <parth.detroja@aliansoftware.com>
2025-12-23 13:48:05 +0530

    Add languages JSON's and integrated

--- frontend/src/locales/main.js
+++ frontend/src/locales/main.js
@@ -1,5 +1,14 @@
 import { createI18n } from "vue-i18n";
 import en from "./en";
+import it from "./it";
+import ge from "./ge";
+import spa from "./spa";
+import gr from "./gr";
+import ch from "./ch";
+import ru from "./ru";
+import hi from "./hi";
+import gu from "./gu";
+import fr from "./fr";
 
 export const i18n = createI18n({
     legacy: false,
@@ -8,5 +17,14 @@ export const i18n = createI18n({
     fallbackLocale: "en",
     messages: {
         en,
+        it,
+        ge,
+        spa,
+        gr,
+        ch,
+        ru,
+        hi,
+        gu,
+        fr
     },
 });
